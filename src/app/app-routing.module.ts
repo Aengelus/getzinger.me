3cc3b4d6dd5369571076153e5512185902d5cbe8
Aengelus <getzinger.sascha@gmail.com>
2018-08-09 12:42:05 +0200

    added router, start page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [];
+
+// You have to include the routes in the module you are using
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+//    useHash: true,
+//    enableTracing: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   imports: [
+    AppRoutingModule,
     CommonModule
   ],
   declarations: [
     LayoutComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   exports: [
     LayoutComponent
