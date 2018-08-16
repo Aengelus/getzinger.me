2a041fcd2b5627d82f05f5e868e6035c5183b386
Aengelus <sascha.getzinger@gmx.de>
2018-08-16 12:47:30 +0200

    Major design updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { HttpClientModule } from '@angular/common/http';
+
+// This is needed to enable the production mode, afterwards you can create your finished packe with 'ng build --prod --build-optimizer'
+// For more information go to 'https://angular.io/guide/deployment'
+//enableProdMode();
 
 @NgModule({
   declarations: [
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
+import Typed from 'typeit';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +12,44 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 export class HomeComponent implements OnInit {
 
+  options = {
+    speed: 100,
+    autoStart: false,
+    afterComplete: function () { this.displayed = true; }
+  };
+  typed: Typed;
+
   constructor (private httpService: HttpClient) { }
   myJson: any = [];
 
   fun: Function = () => console.log('Hello');
 
   ngOnInit () {
+
+    this.typed = new Typed('.type-it', this.options)
+    .type('whoami')
+    .break()
+    .pause(1000)
+    .type('<div><i class="fa fa-spinner fa-pulse fa-fw"></i>')
+    .pause(500)
+    .type('<i class="fa fa-spinner fa-pulse fa-fw"></i>')
+    .pause(500)
+    .type('<i class="fa fa-spinner fa-pulse fa-fw"></i>')
+    .pause(500)
+    .type('<i class="fa fa-spinner fa-pulse fa-fw"></i></div>')
+    .pause(500)
+    .break()
+    .break()
+    .type('<div class="inner"></div>');
+
+    this.typed = new Typed('.test', this.options)
+    .type('Developer')
+    .delete()
+    .type('Financial Analyst')
+    .delete()
+    .type('Cooking Chef')
+    .delete();
+
     this.httpService.get('../assets/data.json').subscribe(
       data => {
         this.myJson = data;	 // FILL THE ARRAY WITH DATA.
@@ -26,6 +59,15 @@ export class HomeComponent implements OnInit {
           console.log (err.message);
       }
     );
+  }
+
+  sendEvent = () => {
+    (<any>window).ga('send', 'event', {
+      eventCategory: 'Video',
+      eventLabel: 'play',
+      eventAction: 'Test Campaign',
+      eventValue: 10
+    });
   }
 
 }
