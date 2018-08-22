65bdba515bfa8a194fcbfb592bc3f00867978c05
Aengelus <sascha.getzinger@gmx.de>
2018-08-22 17:24:55 +0200

    Updated Header, Footer removed, pictures resized

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
-import Typed from 'typeit';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,18 +10,6 @@ import Typed from 'typeit';
 
 export class HomeComponent implements OnInit {
 
-  options = {
-    strings: ['Student', 'Developer', 'Financial Engineer', 'Petroleum Engineer', 'Early Adopter'],
-    speed: 150,
-    autoStart: false,
-//    afterComplete: function () { this.displayed = true; },
-    loop: true,
-    breakLines: false,
-    lifeLike: true,
-    nextStringDelay: 1000
-  };
-  typed: Typed;
-
   constructor (private httpService: HttpClient) { }
   myJson: any = [];
 
@@ -31,27 +17,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit () {
 
-    this.typed = new Typed('.type-it', this.options);
-/*    .pause(2000)
-    .type('Developer')
-    .pause(500)
-    .delete()
-    .type('Student')
-    .pause(500)
-    .delete()
-    .type('Financial Analyst')
-    .delete();*/
-
-    this.httpService.get('../assets/data.json').subscribe(
-      data => {
-        this.myJson = data;	 // FILL THE ARRAY WITH DATA.
-          //console.log(this.myJson);
-      },
-      (err: HttpErrorResponse) => {
-          console.log (err.message);
-      }
-    );
-  }
+  this.httpService.get('../assets/data.json').subscribe(
+    data => {
+      this.myJson = data;	 // FILL THE ARRAY WITH DATA.
+        //console.log(this.myJson);
+        },
+        (err: HttpErrorResponse) => {
+            console.log (err.message);
+        }
+      );
+    }
 
   sendEvent = () => {
     (<any>window).ga('send', 'event', {
